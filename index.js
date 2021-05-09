c7b2b2574e3f48f7a3582743c50a8033664ee8e6
Amlan Chakrabarty <Amlan.Chakrabarty@emc.com>
2021-05-09 15:14:29 +0530

    Changes to integrate telegram

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const https = require("https");
 const openUrl = require("openurl");
 const util = require("util");
 const fs = require("fs");
+const TelegramBot = require('node-telegram-bot-api');
 
 console.log("========================== Welcome to Vaccine Slot Tracker ==========================");
 console.log("==== Developed By -> Amlan Chakrabarty || Email -> Amlan.Chakrabarty15@gmail.com ====");
@@ -20,6 +21,27 @@ function readInputFile() {
 const fileContent = readInputFile();
 const pinCode = fileContent && fileContent.pinCode ? fileContent.pinCode : 722155;
 let reqInterVal = fileContent && fileContent.requestInterval ? fileContent.requestInterval : 15;
+const token = fileContent.telegramBotToken;
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+let chatId = null;
+
+bot.on('message', (msg) => {
+    chatId = msg.chat.id;
+    // console.log("Msg ->", msg.text);
+  
+    getRequestedData(msg.text).then((resp) => {
+        // console.log("Resp ->", resp);
+        const availableCenters = parseCenters(JSON.parse(resp));
+        const statusMsg = availableCenters && availableCenters.length > 0 ? `Slots available in ${msg.text}` : `Sorry No slots at ${msg.text}`;
+        bot.sendMessage(chatId, statusMsg);
+    }).catch(() => {
+        bot.sendMessage(chatId, 'Fail to get details');
+    });
+    // send a message to the chat acknowledging receipt of their message
+   
+  });
 
 if (reqInterVal < 6) {
     console.log("Request Interval Should be above 6. Using 7 as request interval");
@@ -30,9 +52,9 @@ const url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calend
 
 startQuering();
 
-function getRequestedData() {
+function getRequestedData(pin) {
     const date = dateFormat(new Date(), "dd-mm-yyyy");
-    const actualURL = util.format(url, pinCode, date);
+    const actualURL = util.format(url, pin, date);
     return new Promise((resolve, reject) => {
         https.get(actualURL, (res) => {          
             res.on('data', (d) => {
@@ -47,14 +69,18 @@ function getRequestedData() {
 }
 
 async function startQuering() {
-    try {
-        const resp = await getRequestedData();
-        parseResp(JSON.parse(resp));
-    } catch (error) {
-        console.error("Some Error Occured. Skipping this iteration", error)
-    }
+    await getVaccineInfo();
     await delay();
     startQuering();
+}
+
+async function getVaccineInfo() {
+    try {
+        const resp = await getRequestedData(pinCode);
+        parseResp(JSON.parse(resp));
+    } catch (error) {
+        console.error("Some Error Occured. Skipping this iteration", error);
+    }
 }
 
 async function delay(time) {
@@ -64,6 +90,16 @@ async function delay(time) {
 } 
 
 function parseResp(availableCenters) {
+    const availableArray = parseCenters(availableCenters);
+    if (availableArray.length > 0) {
+        console.log("Available Slots->", availableArray);
+        openUrl.open("https://selfregistration.cowin.gov.in/");
+        bot.sendMessage(1888901255, `Slot Details -> ${JSON.stringify(availableArray)}`);
+    } else {
+        console.log("Sorry No Slots available in", pinCode, "at", new Date().toDateString(), new Date().toLocaleTimeString());
+    }
+}
+function parseCenters(availableCenters) {
     const availableArray = [];
     availableCenters.centers.forEach((availableCenter) => {
         availableCenter.sessions.forEach((session) => {
@@ -79,10 +115,5 @@ function parseResp(availableCenters) {
             }
         });
     });
-    if (availableArray.length > 0) {
-        console.log("Available Slots->", availableArray);
-        openUrl.open("https://selfregistration.cowin.gov.in/");
-    } else {
-        console.log("Sorry No Slots available in", pinCode, "at", new Date().toDateString(), new Date().toLocaleTimeString());
-    }
+    return availableArray;
 }
